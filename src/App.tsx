3d35f454b9283d29ffa008160bd77f8fb79ba8b4
hiro8ma <hiro8masu@gmail.com>
2022-05-22 20:46:21 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>hiro8ma</p>
+        <p>
+          <code>hiro8ma</code>
+        </p>
+        <a
+          className="App-link"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.wantedly.com/id/hiro8ma"
+        >
+          wantedly
+        </a>
       </header>
     </div>
   );
